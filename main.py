fb75c50b71aebe7c91b0fba54a91b923e7e122be
GitHexManiac <xXtristheminerXx@gmail.com>
2022-09-20 13:03:42 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+colors = ["red", "blue", "green"]
+red_color = colors = [1]
+print(red_color)
+#I need to look back at the slides/book cuz I don't know how to do this :(
+all_bones = ["maxillary", "tibia", "skull"]
+all_bones.append("fibia")
+print(all_bones)
+#why does it include the thingies uhhh i mean the brackets :((((
