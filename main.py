9dfb14ca4ffd122f76bc45718dc4611a5f105c6f
GitHexManiac <xXtristheminerXx@gmail.com>
2022-09-20 20:51:41 -0400

    bbb

--- main.py
+++ main.py
@@ -4,5 +4,13 @@ print(red_color)
 #I need to look back at the slides/book cuz I don't know how to do this :(
 all_bones = ["maxillary", "tibia", "skull"]
 all_bones.append("fibia")
+all_bones.insert (3, "ulna")
 print(all_bones)
 #why does it include the thingies uhhh i mean the brackets :((((
+top_five_numbers = ["1", "3", "9456", "11", "21"]
+print(top_five_numbers)
+top_five_numbers.remove("11")
+print(top_five_numbers)
+#remember, you can also do top_five_numbers.pop(3)
+#also remember, counting starts at 0
+#also also, when it says index error, it means that it's an error relating to lists.
